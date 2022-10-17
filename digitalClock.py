295ed58b12d97f169e12fafcfc7df52d80965bb9
moisesvillalba <moisesvillalba@gmail.com>
2022-10-17 13:13:03 -0300

    mensaje de bienvenida agregado

--- digitalClock.py
+++ digitalClock.py
@@ -2,4 +2,7 @@ from tkinter import Tk,Label
 window = Tk()
 window.title("Reloj Digital - Digital Clock")
 window.geometry("600x300")
+window.configure(bg="steelblue")
+label = Label(window,text="Welcome",font=("Arial Black",78,"bold"), bg="steelblue" , fg="blue")
+label.pack(pady=100)
 window.mainloop()
